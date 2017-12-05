7d6ce9a9676e078f7045d6536e5f8fbf5b5c30cb
Leif Greiss <helter1234@gmail.com>
2017-12-04 22:40:18 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,73 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# coding: utf-8
+import bs4
+from pandas import DataFrame
+import urllib.request # get webpage
+import csv
+import numpy as np
+import pandas as pd
+for i in range(1, 30): #Number of pages plus
+    if(i==1):
+        url = "http://police.psu.edu/daily-crime-log"
+    else:
+        url = "http://police.psu.edu/daily-crime-log?field_reported_value[value]=&page="+str(i)
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+    # print url
+    # print "#####################"
+    allCrimes = pd.DataFrame(columns = ['Incident#', 'Occurred', 'reported', 'nature of incident', 'offenses', 'location', 'disposition'])
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    source = urllib.request.urlopen(url).read()
+    bs_tree = bs4.BeautifulSoup(source, "lxml")
+
+    # print bs_tree
+    # print "#####################"
+
+    incident_nums = bs_tree.findAll('div',attrs={"class" : "views-field views-field-title"})
+
+    occurred = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-occurred"})
+
+    # print occurred
+    # print "#####################"
+    reported = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-reported"})
+
+    # print reported
+    # print "#####################"
+
+    incidents = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-nature-of-incident"})
+
+    # print incidents
+    # print "#####################"
+
+    offenses = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-offenses"})
+
+    # print offenses
+    # print "#####################"
+
+    locations = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-location"})
+
+    # print locations
+    # print "#####################"
+
+    dispositions = bs_tree.findAll('div',attrs={"class" : "views-field views-field-field-case-disposition"})
+
+    # print dispositions
+    # print "#####################"
+
+    total = len(incident_nums)
+
+    # print total
+    # print "#####################"
+
+    count = 0
+
+    while (count<total):
+        incNum = incident_nums[count].find('span',attrs={"class" : "field-content"}).get_text()
+        occr = occurred[count].find('span',attrs={"class" : "field-content"}).get_text()
+        repo = reported[count].find('span',attrs={"class" : "field-content"}).get_text()
+        incNat = incidents[count].find('span',attrs={"class" : "field-content"}).get_text()
+        offe = offenses[count].find('span',attrs={"class" : "field-content"}).get_text()
+        loca = locations[count].find('span',attrs={"class" : "field-content"}).get_text()
+        disp = dispositions[count].find('span',attrs={"class" : "field-content"}).get_text()
+        allCrimes.loc[count] =[incNum, occr, repo, incNat, offe, loca, disp]
+        with open('New_Crime_Log', 'a') as f:
+            allCrimes.to_csv(f, header=True)
+        count +=1
